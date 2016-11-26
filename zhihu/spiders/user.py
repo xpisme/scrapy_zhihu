fdf3d807617d8f3e12739b4b3c1b011ebde1296a
xpisme <gxpisme@gmail.com>
2016-11-26 22:38:25 +0800

    through topic followers

--- zhihu/spiders/user.py
+++ zhihu/spiders/user.py
@@ -14,40 +14,11 @@ class user(scrapy.Spider):
     base_url = 'https://www.zhihu.com'
     allowed_domains = ["www.zhihu.com"]
     start_urls = [
-       "https://www.zhihu.com/people/whale",
-       "https://www.zhihu.com/people/Dian",
-       "https://www.zhihu.com/people/oliverc",
-       "https://www.zhihu.com/people/jixin",
-       "https://www.zhihu.com/people/fanquanquan",
-       "https://www.zhihu.com/people/amonjok",
-       "https://www.zhihu.com/people/delphi",
-       "https://www.zhihu.com/people/yolfilm",
-       "https://www.zhihu.com/people/yuxinlie",
-       "https://www.zhihu.com/people/fu-er",
-       "https://www.zhihu.com/people/woody",
-       "https://www.zhihu.com/people/zhao-wei",
-       "https://www.zhihu.com/people/vinciarts",
-       "https://www.zhihu.com/people/zhaoyang",
-       "https://www.zhihu.com/people/zhong-jia",
-       "https://www.zhihu.com/people/chengyuan",
-       "https://www.zhihu.com/people/shijun",
-       "https://www.zhihu.com/people/mafada",
-       "https://www.zhihu.com/people/shen-yin",
-       "https://www.zhihu.com/people/he-xiao-fei",
-       "https://www.zhihu.com/people/huo-hua-wu-guo-hong",
-       "https://www.zhihu.com/people/zhao-ya-xing",
-       "https://www.zhihu.com/people/BenInCanada",
-       "https://www.zhihu.com/people/yang-xiao-wei",
-       "https://www.zhihu.com/people/fuyuan",
-       "https://www.zhihu.com/people/webryan",
-       "https://www.zhihu.com/people/gevin",
-       "https://www.zhihu.com/people/qichong",
-       "https://www.zhihu.com/people/learno",
-       "https://www.zhihu.com/people/suchangmao",
+        'https://www.zhihu.com/topic/19552249/followers',
     ]
-    password = ''
+    password = 'guoxinpeng'
     email = ''
-    phone = ''
+    phone = '18811040172'
     headers = {
         'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding':'gzip, deflate, sdch, br',
@@ -57,7 +28,8 @@ class user(scrapy.Spider):
         'Host':'www.zhihu.com',
         'Upgrade-Insecure-Requests':'1',
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36',
-        "Referer": "http://www.zhihu.com/"
+        "Referer": "https://www.zhihu.com",
+        "Origin" : "https://www.zhihu.com"
     }
 
     def start_requests(self):
@@ -65,15 +37,14 @@ class user(scrapy.Spider):
         #重写了爬虫类的方法, 实现了自定义请求, 运行成功后会调用callback回调函数
 
     def post_login(self, response):
-        print response.css('title::text')[0].extract()
         xsrf = response.css('input[name=_xsrf]::attr("value")')[0].extract()
-        time.sleep(randint(1,2))
         if self.phone:
+            print 'xxxxx'
             return scrapy.FormRequest(
                 'http://www.zhihu.com/login/phone_num',
                 meta={'cookiejar': response.meta['cookiejar']},
                 formdata={'_xsrf': xsrf, 'password': self.password, 'remember_me' : "True", 'phone_num':self.phone},
-                callback=self.after_login,
+                callback=self.set_refer,
                 dont_filter=True
             )
         else:
@@ -81,14 +52,60 @@ class user(scrapy.Spider):
                 'http://www.zhihu.com/login/email',
                 meta={'cookiejar': response.meta['cookiejar']},
                 formdata={'_xsrf': xsrf, 'password': self.password, 'captcha_type': 'cn', 'remember_me' : "True", 'email':self.email},
-                callback=self.after_login,
+                callback=self.set_refer,
                 dont_filter=True
             )
 
-    def after_login(self, response):
-        for i in self.start_urls:
+    def set_refer(self, response):
+        print '--------------------set_refer------------------'
+        yield scrapy.Request(url = "https://www.zhihu.com/topic/19552249/followers", meta = {'cookiejar': 1},  callback=self.parse_follower)
+
+    def parse_follower(self, response):
+        scrapy_url = response.url
+        print 'parse topic follower'
+        urls = response.css('.zm-list-avatar-medium::attr("href")').extract()
+        self.parse_urls(urls)
+        print 'start topic json'
+        xsrf = response.css('input[name=_xsrf]::attr("value")')[0].extract()
+        headers = self.headers
+        headers['X-Xsrftoken'] = xsrf
+        print 'start  offset  end'
+        self.start = response.css('.zm-person-item::attr("id")')[-1].extract().encode('utf-8')[3:]
+        self.offset = 40
+        self.end = False
+        print self.start
+
+        while (not self.end) :
+            print 'request json'
+            time.sleep(randint(1, 2))
+            yield scrapy.FormRequest(
+                url = scrapy_url,
+                meta = {'cookiejar': response.meta['cookiejar']},
+                formdata = {'offset' : str(self.offset), 'start' : self.start},
+                callback = self.parse_json,
+                headers = headers,
+                dont_filter = True
+            )
+
+    def parse_urls(self, urls):
+        for url in urls:
+            print 'parsing ', url
             time.sleep(randint(1,2))
-            yield scrapy.Request(url = i, meta = {'cookiejar': response.meta['cookiejar']}, callback = self.parse_item) 
+            #yield scrapy.Request(url = url, callback = self.parse_item) 
+
+    def parse_json(self, response):
+        print 'parsing '
+        print response.url
+        print self.start
+        print self.offset
+        jsonresponse = json.loads(response.body_as_unicode())
+        body = jsonresponse['msg'][1]
+        if jsonresponse['msg'][0] < 20:
+            self.end = True
+        self.offset = self.offset + 20
+        self.start = Selector(text=body).css('.zm-person-item::attr("id")')[-1].extract().encode('utf-8')[3:]
+        urls = Selector(text=body).css('.zm-list-avatar-medium::attr("href")').extract()
+        yield self.parse_urls(urls)
 
     def parse_item(self, response):
         print 'parsing ', response.url
@@ -132,17 +149,9 @@ class user(scrapy.Spider):
         if zhihu_item['id']:
            print '----> db <----'
            write_db = DB(MySQL['db_host'], MySQL['db_port'], MySQL['db_user'], MySQL['db_password'], MySQL['db_dbname']) 
-           sql = """insert ignore into user (u_id, name, avatar, remark, agree, thanks, location, business, gender, employment, education, education_extra, asks, answers, posts, collections, logs) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-           resQuery = write_db.execute(sql, (zhihu_item['id'], zhihu_item['name'], zhihu_item['avatar'], zhihu_item['remark'], zhihu_item['agree'], zhihu_item['thanks'], zhihu_item['location'], zhihu_item['business'], zhihu_item['gender'], zhihu_item['employment'], zhihu_item['education'], zhihu_item['education_extra'], zhihu_item['asks'], zhihu_item['answers'], zhihu_item['posts'], zhihu_item['collections'], zhihu_item['logs']))
+           sql = """insert ignore into user (u_id, name, avatar, remark, agree, thanks, location, business, gender, employment, education, education_extra, asks, answers, posts, collections, logs, url) values (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+           resQuery = write_db.execute(sql, (zhihu_item['id'], zhihu_item['name'], zhihu_item['avatar'], zhihu_item['remark'], zhihu_item['agree'], zhihu_item['thanks'], zhihu_item['location'], zhihu_item['business'], zhihu_item['gender'], zhihu_item['employment'], zhihu_item['education'], zhihu_item['education_extra'], zhihu_item['asks'], zhihu_item['answers'], zhihu_item['posts'], zhihu_item['collections'], zhihu_item['logs'], response.url))
            write_db.__delete__()
            print 'resQuery -------   ', resQuery
 
-        followees_url = response.url + '/followees'
-        yield scrapy.Request(url = followees_url , meta = {'cookiejar': response.meta['cookiejar']}, callback = self.parse_list)
 
-    def parse_list(self, response):
-        url_list = response.css('.zm-item-link-avatar::attr("href")').extract()
-        for i in url_list:
-            time.sleep(randint(1,2))
-            people_url = self.base_url + i
-            yield scrapy.Request(url = people_url, meta = {'cookiejar': response.meta['cookiejar']}, callback = self.parse_item)
